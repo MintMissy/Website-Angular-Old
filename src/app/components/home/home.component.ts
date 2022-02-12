da922b87cbf4996a4f70f1cb238756176af8521f
MintMissy <dawidkostka480@gmail.com>
2022-02-12 23:30:15 +0100

    Add home-hero animations

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('homeHeroH1', { static: true })
+  homeHeroH1!: ElementRef<HTMLHeadingElement>;
+  @ViewChild('homeHeroH2', { static: true })
+  homeHeroH2!: ElementRef<HTMLHeadingElement>;
+  @ViewChild('homeHeroP', { static: true })
+  homeHeroP!: ElementRef<HTMLParagraphElement>;
+  @ViewChild('homeHeroBtn', { static: true })
+  homeHeroBtn!: ElementRef<HTMLButtonElement>;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.slideInAnimation(this.homeHeroH1, 0.5);
+    this.slideInAnimation(this.homeHeroH2, 0.5);
+    this.slideInAnimation(this.homeHeroP, 0.65);
+    this.slideInAnimation(this.homeHeroBtn, 0.75);
   }
 
+  slideInAnimation(element: ElementRef, delay: number) {
+    gsap.from(element.nativeElement, {
+      delay: delay,
+      duration: 0.75,
+      opacity: 0,
+      y: 20,
+    });
+  }
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-navbar',
@@ -6,11 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  @ViewChild('navRightLinks', { static: true })
+  navRightLinks!: ElementRef<HTMLUListElement>;
+  @ViewChild('settingsBtn', { static: true })
+  settingsBtn!: ElementRef<HTMLDivElement>;
+
   isNavbarActive = false;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    gsap.from(this.navRightLinks.nativeElement.childNodes, {
+      delay: 0,
+      duration: 0.75,
+      opacity: 0,
+      x: 50,
+      stagger: 0.15,
+    });
+    gsap.from(this.settingsBtn.nativeElement, {
+      delay: 0.45,
+      duration: 0.75,
+      opacity: 0,
+      x: 50,
+    });
+  }
 
   toggleNavbar() {
     this.isNavbarActive = !this.isNavbarActive;
